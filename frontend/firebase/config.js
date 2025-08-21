6611d5917754227d269bf03d048b31c7b8a305cc
Paratodos.ai <gunther.delrosario@gmail.com>
2025-08-21 16:03:47 -0700

    Use environment variables for Firebase config

--- frontend/firebase/config.js
+++ frontend/firebase/config.js
@@ -5,13 +5,14 @@ import { getFirestore } from "firebase/firestore"
 import { getStorage } from "firebase/storage"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBcrW4SOYXWpYtV9iBcKzN0F01P6EqMHyU",
-  authDomain: "carpihogaria.firebaseapp.com",
-  projectId: "carpihogaria",
-  storageBucket: "carpihogaria.appspot.com",
-  messagingSenderId: "837973633322",
-  appId: "1:837973633322:web:b134e078f7f5159e185653"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
   // measurementId no es necesario para login
+  // measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 }
 
 const app = initializeApp(firebaseConfig)
